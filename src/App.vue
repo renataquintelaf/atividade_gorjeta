1fc75d383ba9da1250e8099da5d502ae086e562f
renataquintelaf <vihquintela0604@gmail.com>
2022-08-25 21:18:35 -0300

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,76 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+const pessoas = ref("");
+const valor = ref("");
+const gorgeta = ref("");
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <h1> Gorjeta</h1>
+    <input type="number" v-model="valor" placeholder="Valor da conta" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <select name="" id="Avaliação" v-model="gorgeta">
+      <option value="1" selected="selected">
+        Selecione a avaliação do atendimento:
+      </option>
+      <option value="05">5% (Péssimo)</option>
+      <option value="10">10% (Ruim)</option>
+      <option value="15">15% (Ok)</option>
+      <option value="20">20% (Bom)</option>
+      <option value="25">25% (Muito bom)</option>
+      <option value="30">30% (Excepcional)</option>
+    </select>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <input type="number" placeholder="Pessoas à pagar" v-model="pessoas" />
+
+    <p>
+      
+      Valor à ser pago: R$
+      {{ Math.ceil(((valor * gorgeta) / pessoas) * 100) / 100 }}
+    </p>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+
+.container {
+  margin: 100px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px transparent;
+  border-radius: 15px;
+  width: 600px;
+  height: 500px;
+  background-color: #AC7088;
+
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+input,
+select,
+p {
+  margin: 20px;
+  width: 280px;
+  height: 45px;
+  border-radius: 25px;
+   border: 1px solid transparent;
 }
+p {
+  border: 1px solid transparent;
+  padding: 2px 6px;
+  background-color: white;
+}
+body {
+  height: 665px;
+  
+}
+*{
+  font-family: 'Pacifico', cursive;
+  font-weight: 100px;
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {@
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+}
+.template {
+  background-color: #DEB6AB;
 }
 </style>
